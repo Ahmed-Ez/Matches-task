aa5a8df98f3b86b50ed1a5dc84a760dd2c188acb
Ahmed Ezzat <moreone25@yahoo.com>
2020-11-20 20:10:43 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
